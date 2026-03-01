3d0916c28fd34f07aa3a2170fd6f1b2b3226c7ea
dkwo575 <dkwo575@aucklanduni.ac.nz>
2026-03-01 14:37:26 +1300

    For security, add .env file and change

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -14,13 +14,13 @@ import { getStorage } from "firebase/storage"; // We will use this later for Ima
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBi8gJIaMm0Ifzz9Tq4YtqJaZhX_bPGWpw",
-    authDomain: "p61-smartfarm.firebaseapp.com",
-    projectId: "p61-smartfarm",
-    storageBucket: "p61-smartfarm.firebasestorage.app",
-    messagingSenderId: "533609614738",
-    appId: "1:533609614738:web:0f2dbdc7481f9e7d78854b",
-    measurementId: "G-49B4M560K5"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
